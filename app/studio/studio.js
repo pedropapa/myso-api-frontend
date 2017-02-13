f30ffad208d86f6bdb2d5fc9c55348d2271d1f90
Pedro Papadópolis <pplp93@gmail.com>
2017-02-12 23:54:20 -0200

    [FIX] Studio CRUD missing image uploaders and fields. - Image uploader was not working on Studio edit form. [FEAT] Ability to create studio's with existing Stripe's api credentials. - Ability to edit studio's stripe api credentials.

--- app/studio/studio.js
+++ app/studio/studio.js
@@ -2,15 +2,15 @@
   "use strict";
 
   angular.module('StudioModule', ['myApp', 'ngRoute'])
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
       $routeProvider.when('/studio', {
         templateUrl: 'studio/show.html',
         controller: 'StudioController',
         resolve: {
-          studios: ['consts', '$http', 'ApiService', function(consts, $http) {
-            return $http.get(consts.apiUrl + '/Studios').then(function(response) {
+          studios: ['consts', '$http', 'ApiService', function (consts, $http) {
+            return $http.get(consts.apiUrl + '/Studios').then(function (response) {
               return response.data;
-            }, function(err) {
+            }, function (err) {
               return false;
             });
           }]
@@ -18,31 +18,31 @@
       });
     }])
 
-    .controller('StudioController', ['$scope', 'studios', 'ngDialog', '$http', 'consts', '$route', function($scope, studios, ngDialog, $http, consts, $route) {
-      $scope.edit = function(studio) {
+    .controller('StudioController', ['$scope', 'studios', 'ngDialog', '$http', 'consts', '$route', function ($scope, studios, ngDialog, $http, consts, $route) {
+      $scope.edit = function (studio) {
         ngDialog.open({
           template: 'studio/edit.html',
           className: 'ngdialog-theme-default',
           width: '70%',
           controller: 'StudioEditController',
           resolve: {
-            studio: [function() {
+            studio: [function () {
               return studio;
             }]
           }
         });
       };
 
-      $scope.remove = function(studio) {
+      $scope.remove = function (studio) {
         ngDialog.openConfirm({
-          template:'\
+          template: '\
                 <p>Do you really want to remove this Studio?</p>\
                 <div class="ngdialog-buttons">\
                     <button type="button" class="ngdialog-button ngdialog-button-secondary" ng-click="closeThisDialog(0)">No</button>\
                     <button type="button" class="ngdialog-button ngdialog-button-primary" ng-click="confirm(1)">Yes</button>\
                 </div>',
           plain: true
-        }).then(function(confirm) {
+        }).then(function (confirm) {
           $http.delete(consts.apiUrl + '/Studios/' + studio.id)
             .then(function (response) {
 
@@ -62,51 +62,26 @@
         });
       }
 
-      $scope.new = function() {
+      $scope.new = function () {
         ngDialog.open({
           template: 'studio/new.html',
           className: 'ngdialog-theme-default',
           width: '70%',
           controller: 'StudioNewController',
-          resolve: {
-
-          }
+          resolve: {}
         });
       };
 
       $scope.studios = studios;
     }])
-    .controller('StudioEditController', ['$scope', 'studio', 'ngDialog', '$http', '$route', 'consts',
-      function($scope, studio, ngDialog, $http, $route, consts) {
-      $scope.submit = function(studio) {
-        $http.post(consts.apiUrl + '/Studios/update', studio, {params: {where: {id: studio.id}}})
-          .then(function (response) {
-
-            noty({type: 'success', text: 'Changes saved '});
-            $scope.closeThisDialog();
-            $route.reload();
-          }, function (err) {
-            if (err.data.error && err.data.error.message) {
-              noty({type: 'error', text: err.data.error.message});
-            } else {
-              console.error(err);
-              noty({
-                type: 'error',
-                text: 'An unknown error has ocurred, check browser\'s console for more info'
-              });
-            }
-          });
-      };
-
-      $scope.studio = angular.copy(studio);
-    }])
-    .controller('StudioNewController', ['$scope', 'ngDialog', '$http', '$route', 'consts', 'Upload',
-      function($scope, ngDialog, $http, $route, consts, Upload) {
+    .controller('StudioEditController', ['$scope', 'studio', 'ngDialog', '$http', '$route', 'consts', 'Upload',
+      function ($scope, studio, ngDialog, $http, $route, consts, Upload) {
         $scope.loginPageBackgroundImage = {};
         $scope.dsPromoImg1 = {};
         $scope.dsPromoImg2 = {};
         $scope.dsPromoImg3 = {};
         $scope.dsAboutUsImage = {};
+        $scope.dsLogo = {};
 
         $scope.upload = function (file, model, formModel, formModelVar) {
           Upload.upload({
@@ -126,7 +101,56 @@
           });
         };
 
-        $scope.submit = function(studio) {
+        $scope.submit = function (studio) {
+          $http.post(consts.apiUrl + '/Studios/update', studio, {params: {where: {id: studio.id}}})
+            .then(function (response) {
+
+              noty({type: 'success', text: 'Changes saved '});
+              $scope.closeThisDialog();
+              $route.reload();
+            }, function (err) {
+              if (err.data.error && err.data.error.message) {
+                noty({type: 'error', text: err.data.error.message});
+              } else {
+                console.error(err);
+                noty({
+                  type: 'error',
+                  text: 'An unknown error has ocurred, check browser\'s console for more info'
+                });
+              }
+            });
+        };
+
+        $scope.studio = angular.copy(studio);
+      }])
+    .controller('StudioNewController', ['$scope', 'ngDialog', '$http', '$route', 'consts', 'Upload',
+      function ($scope, ngDialog, $http, $route, consts, Upload) {
+        $scope.loginPageBackgroundImage = {};
+        $scope.dsPromoImg1 = {};
+        $scope.dsPromoImg2 = {};
+        $scope.dsPromoImg3 = {};
+        $scope.dsAboutUsImage = {};
+        $scope.dsLogo = {};
+
+        $scope.upload = function (file, model, formModel, formModelVar) {
+          Upload.upload({
+            url: consts.apiUrl + '/AWSS3s/' + consts.s3bucket + '/upload',
+            data: {file: file}
+          }).then(function (resp) {
+            model.location = formModel[formModelVar] = consts.apiUrl + '/AWSS3s/' + consts.s3bucket + '/download/' + resp.config.data.file.name;
+          }, function (resp) {
+            noty({
+              type: 'error',
+              text: 'An error has ocurred while uploading the image to the server.'
+            });
+
+            console.error(resp);
+          }, function (evt) {
+            model.progress = parseInt(100.0 * evt.loaded / evt.total);
+          });
+        };
+
+        $scope.submit = function (studio) {
           $http.post(consts.apiUrl + '/Studios', studio)
             .then(function (response) {
 
